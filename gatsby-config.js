20e76cc7a6fbb0527376c555ae8c995ae039b2b2
Erick Garcia <github@erick-garcia.com>
2023-03-08 21:39:45 -0800

    lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,19 +9,9 @@ module.exports = {
         website: {
           title: "My Blog", // Homepage title
           titleShort: "tsundere space", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-          name: "My Blog", // Website name used for homescreen (PWA) and SEO
+          name: "My blog", // Website name used for homescreen (PWA) and SEO
           description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
           language: "en", // Sets the global HTML lang attribute
-          logoUrl: "/logos/logo-1024.png", // Logo used for SEO
-          fbAppId: "1825356251115265", // FB Application ID for using app insights
-          twitterName: "Vagr9K", // Twitter handle of the website
-          url: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without the pathPrefix
-          rss: "/rss.xml", // Path to the RSS file
-          rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-
-          googleAnalyticsId: "UA-VALIDID", // GA tracking ID
-          copyright: "© Copyright 2021 | Advanced User", // Copyright string for the footer of the website and RSS feed.
-
           themeColor: "#D83850", // Used for setting manifest and progress theme colors.
           backgroundColor: "#F7F7F7", // Used for setting manifest background color.
         },
@@ -49,19 +39,7 @@ module.exports = {
         },
 
         // Gatsby Configuration
-        pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-
         contentDir: "/content/", // Directory for MDX posts. Defaults to "content".
-        assetDir: "/static/", // Asset directory. Defaults to "static".
-
-        embeddedImageWidth: 768, // MDX embedded image width. Used by gatsby-plugin-image for optimization
-        embeddedVideoWidth: 920, // MDX embedded video width in pixels
-
-        iconPath: undefined, // Icon used for manifest icon creation.
-        iconList: [], // Icons used for the web manifest. Can be used to override iconPath for a more pixel perfect control.
-        iconCachePaths: undefined, // Glob pattern path for the icons to be cached by the gatsby-plugin-offline
-
-        basePath: undefined, // Base path for mounting pages. Allows for multiple themes to be used in a single website.
       },
     },
   ],
